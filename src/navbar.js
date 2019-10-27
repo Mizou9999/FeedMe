f2e42d4a751933ddc1b0c5028ea60507ee096093
mizou <mizou9999@yandex.ru>
2019-10-27 17:32:21 +0300

    Feed me App

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,7 @@
-
-
-export const navbar = (element) => {
-  
-   
-   const navbar = document.createElement('div')
-    navbar.innerHTML= `<nav class="navbar navbar-expand-lg  bg-primary navbar-dark nav-bar justify-content-center">
-    <a class="navbar-brand" href="#"> <i class="fas fa-utensils logo-text" width="50" height="80" ></i> <span class="logo-text">Eat<span class="me">.Me</span></span>
+export const navbar = element => {
+	const navbar = document.createElement("div");
+	navbar.innerHTML = `<nav class="navbar navbar-expand-lg  bg-primary navbar-dark nav-bar justify-content-center">
+    <a class="navbar-brand" href="#"> <i class="fas fa-utensils logo-text" width="50" height="80" ></i> <span class="logo-text">Feed<span class="me">.Me</span></span>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -14,8 +10,8 @@ export const navbar = (element) => {
       
     </div>
 </nav>
-`
-  element.appendChild(navbar)
-}
+`;
+	element.appendChild(navbar);
+};
 
-//width="50" height="80" 
+//width="50" height="80"
